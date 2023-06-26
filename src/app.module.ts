70c367e7c20bf917e4fb7c194a52009cdebb37d9
Gabriel Camargo <gcamargo.dev@gmail.com>
2023-06-26 20:04:37 -0300

    feat: create typeorm & postgres connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [
+    CoursesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'postgres',
+      autoLoadEntities: true,
+      sychronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

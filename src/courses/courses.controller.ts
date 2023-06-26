33c9f11540755aad7089599d6d07eb63d0e3d195
Gabriel Camargo <gcamargo.dev@gmail.com>
2023-06-26 18:54:39 -0300

    feat: implements courses service

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -2,40 +2,39 @@ import {
   Body,
   Controller,
   Get,
-  HttpCode,
-  HttpStatus,
   Param,
   Patch,
   Delete,
   Post,
-  Res,
 } from '@nestjs/common';
+import { CoursesService } from './courses.service';
 
 @Controller('courses')
 export class CoursesController {
+  constructor(private readonly coursesService: CoursesService) {}
+
   @Get()
-  findAll(@Res() response) {
-    return response.status(200).send('Listagem de cursos');
+  findAll() {
+    return this.coursesService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `Curso #${id}`;
+    return this.coursesService.findOne(id);
   }
 
   @Post()
-  @HttpCode(HttpStatus.NO_CONTENT)
   create(@Body() body) {
-    return body;
+    return this.coursesService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `Atualização do curso #${id}`;
+    return this.coursesService.update(id, body);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string) {
-    return `Deletado curso #${id}`;
+  remove(@Param('id') id: string) {
+    return this.coursesService.remove(id);
   }
 }
